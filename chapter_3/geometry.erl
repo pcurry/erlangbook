2dcf39a29f5bb6ba3b1dd4c30c02636e4ec20db3
Paul Curry <pcurry@gmail.com>
2010-12-18 18:00:14 -0500

    Parentheses typo.  Fixed.

--- chapter_3/geometry.erl
+++ chapter_3/geometry.erl
@@ -1,5 +1,5 @@
 -module(geometry).
 -export([area/1]).
 area({rectangle, Width, Height}) -> Width * Height;
-area(({circle, R})               -> 3.14159 * R * R.
+area({circle, R})                -> 3.14159 * R * R.
 
