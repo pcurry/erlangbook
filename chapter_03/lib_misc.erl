addd20ce4ef6208b8f0d02f81a5790b9b1f96bf6
Paul Curry <pcurry@gmail.com>
2010-12-23 23:13:45 -0500

    Added 'for' to lib_misc, per section 3.4

--- chapter_03/lib_misc.erl
+++ chapter_03/lib_misc.erl
@@ -1,8 +1,10 @@
 -module(lib_misc).
--export([sum/1]).
+-export([sum/1, for/3]).
 
 suum(L)  -> sum(L, 0).
 
 sum([], N)    -> N;
 sum([H|T], N) -> sum(T, H+N).
 
+for(Max, Max, F) -> [F(Max)];
+for(I, Max, F)   -> [F(I)|for(I+1, Max, F)].
