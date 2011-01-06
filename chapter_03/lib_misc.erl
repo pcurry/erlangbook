f01d7130492e36170b0ad45407e6b4678a778f54
Paul Curry <pcurry@gmail.com>
2011-01-06 13:49:02 -0500

    Include the changes to lib_misc.erl from the end of the chapter.

--- chapter_03/lib_misc.erl
+++ chapter_03/lib_misc.erl
@@ -31,3 +31,21 @@ pythag(N) ->
 perms([]) -> [[]];
 perms(L)  -> [[H|T] || H <- L, T <- perms(L--[H])].
 	      
+%% Splits a list of numbers, inefficiently.
+odds_and_evens(L) ->
+    Odds  = [X || X <- L, (X rem 2) =:= 1],
+    Evens = [X || X <- L, (X rem 2) =:= 0],
+    {Odds, Evens}.
+
+
+%% Splits the list of numbers more efficiently, using accumulators.
+odds_and_evens_acc(L) ->
+    odds_and_evens_acc(L, [], []).
+
+odds_and_evens_acc([H|T], Odds, Evens) ->
+    case (H rem 2) of 
+	1 -> odds_and_evens_acc(T, [H|Odds], Evens);
+	0 -> odds_and_evens_acc(T, Odds, [H|Evens]) 
+    end;
+odds_and_evens_acc([], Odds, Evens) -> {Odds, Evens}.
+
