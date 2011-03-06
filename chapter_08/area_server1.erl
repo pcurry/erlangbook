2e8b96f458b955272c54a7ec17a1f906b89c0d6b
Paul Curry <pcurry@gmail.com>
2011-03-05 19:35:38 -0500

    Now in message-passing style!

--- chapter_08/area_server1.erl
+++ chapter_08/area_server1.erl
@@ -12,13 +12,13 @@ rpc(Pid, Request) ->
 loop() ->
     receive
 	{From, {rectangle, Width, Ht}} -> 
-	    io:format("Area of rectangle is ~p~n", [Width * Ht]),
+	    From ! Width * Ht,
 	    loop();
 	{From, {circle, R}} ->
-	    io:format("Area of circle is ~p~n", [3.14159 * R * R]),
+	    From ! 3.14159 * R * R,
 	    loop();
 	Other ->
-	    io:format("I don't know what the area of a ~p is ~n",[Other]),
+	    From ! {self(), {error, Other}},
 	    loop()
     end.
 
