e6d849700f8e23246814a4c30ce206cf5a66c885
Paul Curry <pcurry@gmail.com>
2011-03-11 00:09:20 -0500

    Series of modifications to lib_misc.erl

--- chapter_08/lib_misc.erl
+++ chapter_08/lib_misc.erl
@@ -54,3 +54,30 @@ sqrt(X) when X < 0 ->
     erlang:error({squareRootNegativeArgument, X});
 sqrt(X) -> math:sqrt(X).
 
+
+%%% Chapter 8
+sleep(T) ->
+    receive
+    after T ->
+	    true
+    end.
+
+flush_buffer() ->
+    receive
+	_Any ->
+	    flush_buffer()
+    after 0 ->
+	    true
+    end.
+
+priority_receive() ->
+    receive
+	{alarm, X} ->
+	    {alarm, X}
+    after 0 ->
+	    receive
+		Any ->
+		    Any
+	    end
+    end.
+
