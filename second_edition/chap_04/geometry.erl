416b48ff701c622a496789e49d2ab38ecaa8528c
Paul Curry <pcurry@gmail.com>
2019-12-08 12:12:30 -0500

    Extend area to circles.

--- second_edition/chap_04/geometry.erl
+++ second_edition/chap_04/geometry.erl
@@ -8,4 +8,5 @@ test() ->
 
 
 area({rectangle, Width, Height}) -> Width * Height;
-area({square, Side})             -> Side * Side.
+area({square, Side})             -> Side * Side;
+area({circle, Radius})           -> 3.14159 * Radius * Radius.
